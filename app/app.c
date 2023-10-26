16bcae8d6fc0573c5467e6142c682de7ce8fcbb4
Chris <write2recon@gmail.com>
2023-10-26 09:34:25 +0200

    DTMF Sidetone 1050 Hz

--- app/app.c
+++ app/app.c
@@ -2294,7 +2294,7 @@ static void APP_ProcessKey(KEY_Code_t Key, bool bKeyPressed, bool bKeyHeld)
 					BK4819_DisableScramble();
 				
 					if (Code == 0xFE)
-						BK4819_TransmitTone(gEeprom.DTMF_SIDE_TONE, 1750);
+						BK4819_TransmitTone(gEeprom.DTMF_SIDE_TONE, 1050); //1750 to 1050 NOAA
 					else
 						BK4819_PlayDTMFEx(gEeprom.DTMF_SIDE_TONE, Code);
 				}
